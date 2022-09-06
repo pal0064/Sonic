f5685c9cd883b38a783438916129d6ea058a809c
pal <pal@dhcp-10-132-132-232.uawifi.arizona.edu>
2022-09-06 14:21:25 -0700

    fix(core): fixed branch name issue

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,7 +11,7 @@ pip_only_packages = [
     'pre-commit',
 ]
 
-dependencies = '{{ cookiecutter.dependency_file }}'
+dependencies = 'requirements.txt'
 
 def write_dependencies():
     with open(dependencies, 'w') as f:
@@ -28,10 +28,8 @@ def write_dependencies():
 write_dependencies()
 remote = '{{cookiecutter.git_remote}}'
 branch_name = 'feat/initialized-using-cookiecutter'
-subprocess.call(['git', 'init'])
-subprocess.call(['git', 'add', '.'])
-subprocess.call(['git', 'commit', '-m', 'initializatin using cookiecutter'])
 subprocess.call(['git', 'checkout', '-b', branch_name])
+subprocess.call(['git', 'add', '.'])
+subprocess.call(['git', 'commit', '-m', 'Initialization using cookiecutter'])
 if remote != 'Git remote (if known)':
-    subprocess.call(['git', 'remote', 'add', 'origin', remote])
     subprocess.call(['git', 'push', '-u', 'origin', branch_name])
